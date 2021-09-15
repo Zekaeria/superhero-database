ca6012104cefc262a654f3bb04df7f3b88b9141a
Zekaeria <87617828+Zekaeria@users.noreply.github.com>
2021-09-15 07:40:00 -0700

    update SearchResult.js

--- src/components/superheroes/SearchResult.js
+++ src/components/superheroes/SearchResult.js
@@ -1,30 +1,25 @@
-
 import React from 'react';
-//import SuperheroList from './SuperheroList';
 
+function SearchResultItem(props) {
+  const {data} = props;
 
-//prints out short/PREVIEW info from search for superhero
-
-function SearchResult(props){
-    const {data} = props;
-
-    console.log('data', data);
-    return (
-        <div className = "search-result">
-            <div className = "left">
-                <img src={data.image.url} alt="super pic" />
-            </div>
-            <div className = "right">
-                <h1>{data.name}</h1>
-                <span style = {{color:'gray', marginBottom: 5}}>{data.biography['full-name']}</span>
-                <div className = "stats" >
-                    <div>Strength: {data.powerstats.strength}</div> 
-                    <div>Speed: {data.powerstats.speed}</div> 
-                    <div>Power: {data.powerstats.power}</div> 
-                </div>
-            </div>      
+  console.log('data', data);
+  return (
+    <div className="search-result">
+      <div className="left">
+        <img src={data.image.url} alt="super pic" />
+      </div>
+      <div className="right">
+        <h1>{data.name}</h1>
+        <span style={{ color:'gray', marginBottom: 5 }}>{data.biography['full-name']}</span>
+        <div className="stats">
+          <div>strength: {data.powerstats.strength}</div>
+          <div>speed: {data.powerstats.speed}</div>
+          <div>power: {data.powerstats.power}</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default SearchResult;
+export default SearchResultItem;
